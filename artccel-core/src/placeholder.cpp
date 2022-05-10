f8a4fa9a541b68fbb859b1194ed0224eb1f94d89
William So <polyipseity@gmail.com>
2022-05-10 23:25:09 +0800

    Setup CMake for root project and 'artccel-core/'
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ artccel-core/src/placeholder.cpp
@@ -0,0 +1,1 @@
+#include "artccel-core/placeholder.hpp"
---
+++ in/Config.cmake
@@ -0,0 +1,4 @@
+@PACKAGE_INIT@
+
+include("${CMAKE_CURRENT_LIST_DIR}/artccel-targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/artccel-core-targets.cmake")
