142e19efb96de6022c8bd6b054bb61007f85b4ad
William So <polyipseity@gmail.com>
2022-05-15 20:00:45 +0800

    Fix 'artccel-core/util/interval.hpp'
    - Fix 1 constructor
    - Replace '()'-initialization with '{}'-initialization
    - Lowercase enumerators
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/interval.hpp
+++ artccel-core/include/artccel-core/util/interval.hpp
@@ -8,29 +8,29 @@ namespace artccel::core {
 template <typename Type> constexpr Type unbounded{};
 
 enum class bound : uint8_t {
-  OPEN,
-  CLOSED,
-  UNBOUNDED,
+  open,
+  closed,
+  unbounded,
 };
 
 template <bound Bound, std::totally_ordered auto Left,
           std::totally_ordered auto Right>
 consteval bool bound_less_than() {
-  return (Bound == bound::OPEN && Left < Right) ||
-         (Bound == bound::CLOSED && Left <= Right) || Bound == bound::UNBOUNDED;
+  return (Bound == bound::open && Left < Right) ||
+         (Bound == bound::closed && Left <= Right) || Bound == bound::unbounded;
 }
 
 inline bool bound_less_than(bound bound, std::totally_ordered auto const &left,
                             std::totally_ordered auto const &right) {
-  return (bound == bound::OPEN && left < right) ||
-         (bound == bound::CLOSED && left <= right) || bound == bound::UNBOUNDED;
+  return (bound == bound::open && left < right) ||
+         (bound == bound::closed && left <= right) || bound == bound::unbounded;
 }
 
 template <std::totally_ordered Type, bound LeftBound, Type Left, Type Right,
           bound RightBound>
 struct interval {
-  interval(Type const &value) : interval(value) {}
-  interval(Type &&value) : value(value) {
+  interval(Type const &value) : interval{Type{value}} {}
+  interval(Type &&value) : value{value} {
     assert(("left >(=) value", bound_less_than(LeftBound, Left, this->value)));
     assert(
         ("value >(=) right", bound_less_than(RightBound, this->value, Right)));
@@ -39,26 +39,26 @@ struct interval {
     static_assert(bound_less_than<LeftBound, Left, Value>(), "left >(=) value");
     static_assert(bound_less_than<RightBound, Value, Right>(),
                   "value >(=) right");
-    return interval(Value, false);
+    return interval{Value, false};
   }
   inline operator Type() { return value; }
   inline operator Type const() const { return value; }
 
 private:
   Type value;
-  interval(Type &&value, bool) : value(value) {}
+  interval(Type &&value, bool) : value{value} {}
 };
 
 template <std::totally_ordered Type>
 using nonnegative_interval =
-    interval<Type, bound::CLOSED, Type{0}, unbounded<Type>, bound::UNBOUNDED>;
+    interval<Type, bound::closed, Type{0}, unbounded<Type>, bound::unbounded>;
 template <std::totally_ordered Type>
 using nonpositive_interval =
-    interval<Type, bound::UNBOUNDED, unbounded<Type>, Type{0}, bound::CLOSED>;
+    interval<Type, bound::unbounded, unbounded<Type>, Type{0}, bound::closed>;
 template <std::totally_ordered Type>
 using positive_interval =
-    interval<Type, bound::OPEN, Type{0}, unbounded<Type>, bound::UNBOUNDED>;
+    interval<Type, bound::open, Type{0}, unbounded<Type>, bound::unbounded>;
 template <std::totally_ordered Type>
 using negative_interval =
-    interval<Type, bound::UNBOUNDED, unbounded<Type>, Type{0}, bound::OPEN>;
+    interval<Type, bound::unbounded, unbounded<Type>, Type{0}, bound::open>;
 } // namespace artccel::core
