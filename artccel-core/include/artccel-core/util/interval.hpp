d67deaf9ad4509c130f19bfec58dc76817bf57d2
William So <polyipseity@gmail.com>
2022-05-15 21:30:16 +0800

    Improve 'artccel-core/util/interval.hpp'
    - Add 'requires'
    - Redirect 1 constructor
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/interval.hpp
+++ artccel-core/include/artccel-core/util/interval.hpp
@@ -3,9 +3,12 @@
 #include <assert.h>
 #include <concepts>
 #include <inttypes.h>
+#include <utility>
 
 namespace artccel::core {
-template <typename Type> constexpr Type unbounded{};
+template <std::totally_ordered Type>
+requires std::default_initializable<Type>
+constexpr Type unbounded{};
 
 enum class bound : uint8_t {
   open,
@@ -15,22 +18,26 @@ enum class bound : uint8_t {
 
 template <bound Bound, std::totally_ordered auto Left,
           std::totally_ordered auto Right>
+requires std::totally_ordered_with<decltype(Left), decltype(Right)>
 consteval bool bound_less_than() {
   return (Bound == bound::open && Left < Right) ||
          (Bound == bound::closed && Left <= Right) || Bound == bound::unbounded;
 }
 
 inline bool bound_less_than(bound bound, std::totally_ordered auto const &left,
-                            std::totally_ordered auto const &right) {
+                            std::totally_ordered auto const &right) requires
+    std::totally_ordered_with<decltype(left), decltype(right)> {
   return (bound == bound::open && left < right) ||
          (bound == bound::closed && left <= right) || bound == bound::unbounded;
 }
 
 template <std::totally_ordered Type, bound LeftBound, Type Left, Type Right,
           bound RightBound>
+requires std::movable<Type>
 struct interval {
-  interval(Type const &value) : interval{Type{value}} {}
-  interval(Type &&value) : value{value} {
+  interval(Type const &value) requires std::copyable<Type>
+      : interval{Type{value}} {}
+  interval(Type &&value) : interval{std::move(value), false} {
     assert(("left >(=) value", bound_less_than(LeftBound, Left, this->value)));
     assert(
         ("value >(=) right", bound_less_than(RightBound, this->value, Right)));
@@ -50,15 +57,19 @@ private:
 };
 
 template <std::totally_ordered Type>
+requires std::constructible_from<Type, decltype(0)>
 using nonnegative_interval =
     interval<Type, bound::closed, Type{0}, unbounded<Type>, bound::unbounded>;
 template <std::totally_ordered Type>
+requires std::constructible_from<Type, decltype(0)>
 using nonpositive_interval =
     interval<Type, bound::unbounded, unbounded<Type>, Type{0}, bound::closed>;
 template <std::totally_ordered Type>
+requires std::constructible_from<Type, decltype(0)>
 using positive_interval =
     interval<Type, bound::open, Type{0}, unbounded<Type>, bound::unbounded>;
 template <std::totally_ordered Type>
+requires std::constructible_from<Type, decltype(0)>
 using negative_interval =
     interval<Type, bound::unbounded, unbounded<Type>, Type{0}, bound::open>;
 } // namespace artccel::core
