420ea3d903fc0c2f1056674fe61c0cfadd656e94
William So <polyipseity@gmail.com>
2022-05-17 23:25:50 +0800

    Improve 'exe/main.cpp'
    - Disable clang-tidy checks 'fuchsia-default-arguments-calls' and 'fuchsia-default-arguments-declarations'
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/src/exe/main.cpp
+++ artccel-core/src/exe/main.cpp
@@ -1,3 +1,19 @@
-auto main(int argc [[maybe_unused]], char *argv [[maybe_unused]][]) -> int {
+#include <gsl/gsl>
+#include <span>
+#include <string_view>
+#include <vector>
+
+namespace artccel {
+auto main(std::span<std::string_view const> arguments [[maybe_unused]]) {
   return 0;
+}
+} // namespace artccel
+
+auto main(int argc, gsl::zstring argv[]) -> int {
+  std::vector<std::string_view> arguments(argc);
+  for (gsl::index i = 0; i < argc; ++i) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    arguments.emplace_back(argv[i]);
+  }
+  return artccel::main(arguments);
 }
