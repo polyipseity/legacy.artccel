088d8c40230e5596df6ea99f900e333ac3aa05f3
William So <polyipseity@gmail.com>
2022-05-22 16:06:17 +0800

    Create 'artccel-core/semantics.hpp'
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ artccel-core/include/artccel-core/semantics.hpp
@@ -0,0 +1,15 @@
+#ifndef ARTCCEL_CORE_SEMANTICS_HPP
+#define ARTCCEL_CORE_SEMANTICS_HPP
+#pragma once
+
+#include <gsl/gsl>
+
+namespace artccel::core::util {
+using Cu8zstring = gsl::basic_zstring<char8_t const, gsl::dynamic_extent>;
+using U8zstring = gsl::basic_zstring<char8_t, gsl::dynamic_extent>;
+
+template <typename T> using Observer_ptr = T *;
+template <typename T> using Observer_ref = T &;
+} // namespace artccel::core::util
+
+#endif
