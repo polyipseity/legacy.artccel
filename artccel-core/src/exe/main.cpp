c5aa7b4002186a41b366cc95891af2fbb35bcc2c
William So <polyipseity@gmail.com>
2022-05-22 10:45:01 +0800

    Invoke 'std::ios_base::sync_with_stdio(false)' in 'exe/main.cpp'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/src/exe/main.cpp
+++ artccel-core/src/exe/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <clocale>
 #include <gsl/gsl>
+#include <iostream>
 #include <span>
 #include <string_view>
 #include <vector>
@@ -8,6 +9,7 @@
 namespace artccel {
 auto main(std::span<std::string_view const> arguments [[maybe_unused]]) {
   std::setlocale(LC_ALL, "C.UTF-8"); // NOLINT(concurrency-mt-unsafe)
+  std::ios_base::sync_with_stdio(false);
   return 0;
 }
 } // namespace artccel
