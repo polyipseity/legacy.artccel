36a28d4b7404ce6a09839562ac57f9c31e840818
William So <polyipseity@gmail.com>
2022-05-22 19:26:47 +0800

    Refactor 'artccel::main' to 'artccel::core::main' in 'exe/main.cpp'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/src/exe/main.cpp
+++ artccel-core/src/exe/main.cpp
@@ -6,18 +6,18 @@
 #include <string_view>
 #include <vector>
 
-namespace artccel {
+namespace artccel::core {
 auto main(std::span<std::string_view const> arguments [[maybe_unused]]) {
   std::setlocale(LC_ALL, "C.UTF-8"); // NOLINT(concurrency-mt-unsafe)
   std::ios_base::sync_with_stdio(false);
   return 0;
 }
-} // namespace artccel
+} // namespace artccel::core
 
 auto main(int argc, gsl::zstring argv[]) -> int {
   std::vector<std::string_view> arguments(argc);
   std::transform(
       argv, argv + argc, arguments.begin(),
       [](gsl::zstring argument) { return std::string_view{argument}; });
-  return artccel::main(arguments);
+  return artccel::core::main(arguments);
 }
