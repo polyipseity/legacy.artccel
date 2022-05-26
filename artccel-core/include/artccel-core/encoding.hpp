faddcb2ff345462d1b8f49e2622e023b41896a9d
William So <polyipseity@gmail.com>
2022-05-26 18:56:16 +0800

    Add new functions in 'artccel-core/encoding.hpp'
    - Fix '#include's comment blunders
    - Improve 'encoding.cpp' using templates
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/encoding.hpp
+++ artccel-core/include/artccel-core/encoding.hpp
@@ -4,8 +4,8 @@
 
 #include "polyfill.hpp" // import literals::operator""_UZ
 #include <cwchar>       // import std::mbstate_t, std::size_t
-#include <string> // import std::string_view, std::u16string_view, std::u8string_view
-#include <string_view> // import std::string, std::u16string, std::u8string
+#include <string> // import std::string, std::u16string, std::u32string, std::u8string
+#include <string_view> // import std::string_view, std::u16string_view, std::u32string_view, std::u8string_view
 
 namespace artccel::core::util {
 constexpr auto cwchar_mbrlen_null{0_UZ};
@@ -26,8 +26,10 @@ auto c16srtoc8s(std::u16string_view c16s) -> std::u8string;
 
 auto mbsrtoc8s(std::string_view mbs) -> std::u8string;
 auto mbsrtoc16s(std::string_view mbs) -> std::u16string;
+auto mbsrtoc32s(std::string_view mbs) -> std::u32string;
 auto c8srtombs(std::u8string_view c8s) -> std::string;
 auto c16srtombs(std::u16string_view c16s) -> std::string;
+auto c32srtombs(std::u32string_view c32s) -> std::string;
 
 auto mbrlen_null(std::string_view mbs, std::mbstate_t &state) -> std::size_t;
 } // namespace artccel::core::util
--- artccel-core/sources/encoding.cpp
+++ artccel-core/sources/encoding.cpp
@@ -7,49 +7,53 @@
 #include <climits>                   // import MB_LEN_MAX
 #include <codecvt>                   // import std::codecvt_utf8_utf16
 #include <cstring>                   // import std::strerror
-#include <cuchar>                    // import std::c16rtomb, std::mbrtoc16
-#include <cwchar>                    // import std::mbrlen, std::mbstate_t, std::size_t
-#include <gsl/gsl>                   // import gsl::czstring
-#include <locale>                    // import std::wstring_convert
-#include <stdexcept>                 // import std::invalid_argument
-#include <string> // import std::string_view, std::u16string_view, std::u8string_view
-#include <string_view> // import std::string, std::u16string, std::u8string
+#include <cuchar> // import std::c16rtomb, std::c32rtomb, std::mbrtoc16, std::mbrtoc32
+#include <cwchar>    // import std::mbrlen, std::mbstate_t, std::size_t
+#include <gsl/gsl>   // import gsl::czstring
+#include <locale>    // import std::wstring_convert
+#include <span>      // import std::span
+#include <stdexcept> // import std::invalid_argument
+#include <string> // import std::basic_string, std::string, std::u16string, std::u32string, std::u8string
+#include <string_view> // import std::basic_string_view, std::string_view, std::u16string_view, std::u32string_view, std::u8string_view
 
 namespace artccel::core::util {
-auto c8s_compatrtoc8s(std::string_view c8s_compat) -> std::u8string {
-  // undefined behavior??? (but no reinterpret_cast)
-  return {c8s_compat.begin(), c8s_compat.end()};
+namespace detail {
+template <typename CharT>
+auto mbrtoc(CharT &out, std::string_view in, std::mbstate_t &state)
+    -> std::size_t;
+template <>
+auto mbrtoc(char16_t &out, std::string_view in, std::mbstate_t &state)
+    -> std::size_t {
+  return std::mbrtoc16(&out, in.data(), in.size(), &state);
 }
-auto c8srtoc8s_compat(std::u8string_view c8s) -> std::string {
-  return {c8s.begin(), c8s.end()};
-}
-
-auto c8srtoc16s(std::u8string_view c8s) -> std::u16string {
-  return std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
-      .from_bytes(
-          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-          reinterpret_cast<char const *>(c8s.begin()), // defined behavior
-          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-          reinterpret_cast<char const *>(c8s.end())); // defined behavior
-}
-auto c16srtoc8s(std::u16string_view c16s) -> std::u8string {
-  return c8s_compatrtoc8s(
-      std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
-          .to_bytes(c16s.begin(), c16s.end()));
+template <>
+auto mbrtoc(char32_t &out, std::string_view in, std::mbstate_t &state)
+    -> std::size_t {
+  return std::mbrtoc32(&out, in.data(), in.size(), &state);
 }
 
-auto mbsrtoc8s(std::string_view mbs) -> std::u8string {
-  // TODO: use std::mbrtoc8
-  return c16srtoc8s(mbsrtoc16s(mbs));
+template <typename CharT>
+auto crtomb(std::span<char, MB_LEN_MAX> out, CharT in, std::mbstate_t &state)
+    -> std::size_t;
+template <>
+auto crtomb(std::span<char, MB_LEN_MAX> out, char16_t in, std::mbstate_t &state)
+    -> std::size_t {
+  return std::c16rtomb(out.data(), in, &state);
 }
-auto mbsrtoc16s(std::string_view mbs) -> std::u16string {
-  std::u16string result{};
+template <>
+auto crtomb(std::span<char, MB_LEN_MAX> out, char32_t in, std::mbstate_t &state)
+    -> std::size_t {
+  return std::c32rtomb(out.data(), in, &state);
+}
+
+template <typename CharT>
+auto mbsrtocs(std::string_view mbs) -> std::basic_string<CharT> {
+  std::basic_string<CharT> result{};
   std::mbstate_t state{};
   while (!mbs.empty()) {
     auto old_state{state};
-    auto c16{
-        u'\0'}; // not written by std::mbrtoc16 if the next character is null
-    auto processed{std::mbrtoc16(&c16, mbs.begin(), mbs.size(), &state)};
+    CharT c{u8'\0'}; // not written to if the next character is null
+    auto processed{mbrtoc(c, mbs, state)};
     switch (processed) {
       [[unlikely]] case cuchar_mbrtoc_error :
           // NOLINTNEXTLINE(concurrency-mt-unsafe)
@@ -76,25 +80,20 @@ auto mbsrtoc16s(std::string_view mbs) -> std::u16string {
       mbs.remove_prefix(processed);
       break;
     }
-    result.push_back(c16);
+    result.push_back(c);
   }
-  for (char16_t c16{}; std::mbrtoc16(&c16, mbs.begin(), mbs.size(), &state) ==
-                       cuchar_mbrtoc_surrogate;) {
-    result.push_back(c16); // complete surrogate pair of the last character
+  for (CharT c{}; mbrtoc(c, mbs, state) == cuchar_mbrtoc_surrogate;) {
+    result.push_back(c); // complete surrogate pair of the last character
   }
   return result;
 }
-
-auto c8srtombs(std::u8string_view c8s) -> std::string {
-  // TODO: use std::c8rtomb
-  return c16srtombs(c8srtoc16s(c8s));
-}
-auto c16srtombs(std::u16string_view c16s) -> std::string {
+template <typename CharT>
+auto csrtombs(std::basic_string_view<CharT> cs) -> std::string {
   std::string result{};
   std::mbstate_t state{};
   std::array<char, MB_LEN_MAX> mb{};
-  for (auto const c16 : c16s) {
-    auto const processed{std::c16rtomb(mb.data(), c16, &state)};
+  for (auto const c : cs) {
+    auto const processed{crtomb(mb, c, state)};
     switch (processed) {
       [[unlikely]] case cuchar_crtomb_error :
           // NOLINTNEXTLINE(concurrency-mt-unsafe)
@@ -106,6 +105,50 @@ auto c16srtombs(std::u16string_view c16s) -> std::string {
     }
   }
   return result;
+}
+} // namespace detail
+
+auto c8s_compatrtoc8s(std::string_view c8s_compat) -> std::u8string {
+  return {c8s_compat.begin(), c8s_compat.end()};
+}
+auto c8srtoc8s_compat(std::u8string_view c8s) -> std::string {
+  return {c8s.begin(), c8s.end()};
+}
+
+auto c8srtoc16s(std::u8string_view c8s) -> std::u16string {
+  return std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
+      .from_bytes(
+          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+          reinterpret_cast<char const *>(c8s.begin()), // defined behavior
+          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+          reinterpret_cast<char const *>(c8s.end())); // defined behavior
+}
+auto c16srtoc8s(std::u16string_view c16s) -> std::u8string {
+  return c8s_compatrtoc8s(
+      std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
+          .to_bytes(c16s.begin(), c16s.end()));
+}
+
+auto mbsrtoc8s(std::string_view mbs) -> std::u8string {
+  // TODO: use std::mbrtoc8
+  return c16srtoc8s(mbsrtoc16s(mbs));
+}
+auto mbsrtoc16s(std::string_view mbs) -> std::u16string {
+  return detail::mbsrtocs<char16_t>(mbs);
+}
+auto mbsrtoc32s(std::string_view mbs) -> std::u32string {
+  return detail::mbsrtocs<char32_t>(mbs);
+}
+
+auto c8srtombs(std::u8string_view c8s) -> std::string {
+  // TODO: use std::c8rtomb
+  return c16srtombs(c8srtoc16s(c8s));
+}
+auto c16srtombs(std::u16string_view c16s) -> std::string {
+  return detail::csrtombs(c16s);
+}
+auto c32srtombs(std::u32string_view c32s) -> std::string {
+  return detail::csrtombs(c32s);
 }
 
 auto mbrlen_null(std::string_view mbs, std::mbstate_t &state) -> std::size_t {
