05238e419fece684a0ad890b1d88832350558bda
William So <polyipseity@gmail.com>
2022-05-29 23:40:11 +0800

    Create 'artccel::core::util::to_underlying' in 'artccel-core/polyfill.hpp'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/polyfill.hpp
+++ artccel-core/include/artccel-core/polyfill.hpp
@@ -2,13 +2,19 @@
 #define ARTCCEL_CORE_POLYFILL_HPP
 #pragma once
 
-#include <cstddef> // import std::size_t
+#include <cstddef>     // import std::size_t
+#include <type_traits> // import std::underlying_type_t
 
 namespace artccel::core::util {
+constexpr auto to_underlying [[nodiscard]] (auto e) noexcept {
+  // TODO: C++23: std::to_underlying
+  return static_cast<std::underlying_type_t<decltype(e)>>(e);
+}
+
 inline namespace literals {
 // NOLINTNEXTLINE(google-runtime-int): specs requires 'unsigned long long'
-consteval auto operator""_UZ(unsigned long long value) noexcept {
-  return static_cast<std::size_t>(value);
+consteval auto operator""_UZ [[nodiscard]](unsigned long long value) noexcept {
+  return static_cast<std::size_t>(value); // TODO: C++23: UZ
 }
 } // namespace literals
 } // namespace artccel::core::util
