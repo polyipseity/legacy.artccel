d5114b609f03f3a55d47a1eda118dec49a0609d9
William So <polyipseity@gmail.com>
2022-05-30 16:21:01 +0800

    Replace 1 unnecessary 'static_cast'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/sources/encoding.cpp
+++ artccel-core/sources/encoding.cpp
@@ -156,8 +156,7 @@ auto mbrlen_null(std::string_view mbs, std::mbstate_t &state) -> std::size_t {
   // NOLINTNEXTLINE(concurrency-mt-unsafe)
   auto const result{std::mbrlen(mbs.begin(), mbs.size(), &state)};
   [[unlikely]] if (result == cwchar_mbrlen_null) {
-    auto const null_len_max{
-        std::min(static_cast<std::size_t>(MB_LEN_MAX), mbs.size())};
+    auto const null_len_max{std::min(std::size_t{MB_LEN_MAX}, mbs.size())};
     for (auto null_len{1_UZ}; null_len <= null_len_max; ++null_len) {
       auto state_copy{old_state};
       // NOLINTNEXTLINE(concurrency-mt-unsafe)
