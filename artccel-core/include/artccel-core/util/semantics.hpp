b0dd9c7f97b24f79c93d2458790404b528606ac4
William So <polyipseity@gmail.com>
2022-06-10 16:07:47 +0800

    Add 'artccel::core::util::Owner' for owning references
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/semantics.hpp
+++ artccel-core/include/artccel-core/util/semantics.hpp
@@ -2,11 +2,16 @@
 #define ARTCCEL_CORE_UTIL_SEMANTICS_HPP
 #pragma once
 
-#include <gsl/gsl> // import gsl::basic_zstring, gsl::dynamic_extent
+#include <gsl/gsl>     // import gsl::basic_zstring, gsl::dynamic_extent
+#include <type_traits> // import std::is_reference_v
 
 namespace artccel::core::util {
 using Cu8zstring = gsl::basic_zstring<char8_t const, gsl::dynamic_extent>;
 using U8zstring = gsl::basic_zstring<char8_t, gsl::dynamic_extent>;
+
+template <typename T>
+requires std::is_reference_v<T>
+using Owner = T;
 
 template <typename T> using Observer_ptr = T *;
 template <typename T> using Observer_ref = T &;
