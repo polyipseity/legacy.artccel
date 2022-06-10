669f9ceb4b71592f8853179868d687f36ec6dc38
William So <polyipseity@gmail.com>
2022-06-10 20:49:49 +0800

    Add 'artccel::core::util::Remove_cvptr'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/meta.hpp
+++ artccel-core/include/artccel-core/util/meta.hpp
@@ -2,6 +2,8 @@
 #define ARTCCEL_CORE_UTIL_META_HPP
 #pragma once
 
+#include <type_traits> // import std::remove_cv_t
+
 namespace artccel::core::util {
 template <typename P> struct Remove_ptr;
 // NOLINTNEXTLINE(altera-struct-pack-align)
@@ -16,6 +18,12 @@ struct Remove_ptr<P<T>> {
   using type = T;
 };
 template <typename P> using Remove_ptr_t = typename Remove_ptr<P>::type;
+
+// NOLINTNEXTLINE(altera-struct-pack-align)
+template <typename P> struct Remove_cvptr {
+  using type = std::remove_cv_t<Remove_ptr_t<P>>;
+};
+template <typename P> using Remove_cvptr_t = typename Remove_cvptr<P>::type;
 
 template <typename P, typename R> struct Replace_ptr_value_type;
 // NOLINTNEXTLINE(altera-struct-pack-align)
