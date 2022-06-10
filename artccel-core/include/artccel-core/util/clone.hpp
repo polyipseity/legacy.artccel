2918491644182f6397482644c4b9602c25e35edb
William So <polyipseity@gmail.com>
2022-06-11 00:24:03 +0800

    Improve 'artccel-core/util/clone.hpp'
    - Fix missing 'std::forward's
    - Improve handling of smart pointers
    - Only allow 'std::shared_ptr's on cloning 'std::enable_shared_from_this's
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/clone.hpp
+++ artccel-core/include/artccel-core/util/clone.hpp
@@ -5,9 +5,9 @@
 #include "encoding.hpp" // import c8srtombs
 #include "meta.hpp"     // import Remove_cvptr_t, Replace_ptr_value_type_t
 #include <cassert>      // import assert
-#include <concepts>     // import std::convertible_to
+#include <concepts>     // import std::convertible_to, std::derived_from
 #include <exception>    // import std::invalid_argument
-#include <memory>       // import std::shared_ptr, std::unique_ptr
+#include <memory> // import std::enable_shared_from_this, std::shared_ptr, std::unique_ptr
 #include <type_traits> // import std::is_pointer_v, std::is_reference_v, std::remove_reference_t
 #include <utility> // import std::declval, std::forward
 
@@ -22,17 +22,25 @@ namespace detail {
   template <cloneable P>
   constexpr auto clone_raw [[deprecated, nodiscard]] (P && ptr)->auto & {
     if constexpr (std::is_reference_v<P>) {
-      if constexpr (std::is_reference_v<decltype(ptr.clone())>) {
+      using return_type = decltype(ptr.clone());
+      if constexpr (std::is_pointer_v<return_type>) {
+        return *(ptr.clone());
+      } else if constexpr (std::is_reference_v<return_type>) {
         return ptr.clone();
       } else {
-        return *(ptr.clone());
+        return ptr.clone().release();
       }
     } else {
-      assert(ptr && u8"ptr == nullptr");
-      if constexpr (std::is_reference_v<decltype(ptr->clone())>) {
+      // clang-format off
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay, hicpp-no-array-decay)
+      /* clang-format on */ assert(ptr && u8"ptr == nullptr");
+      using return_type = decltype(ptr->clone());
+      if constexpr (std::is_pointer_v<return_type>) {
+        return *(ptr->clone());
+      } else if constexpr (std::is_reference_v<return_type>) {
         return ptr->clone();
       } else {
-        return *(ptr->clone());
+        return ptr->clone().release();
       }
     }
   }
@@ -44,7 +52,11 @@ template <
         RP, std::remove_reference_t<
                 // NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations)
                 decltype(detail::clone_raw(std::declval<P>()))>>>
-constexpr auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] (P &&ptr) -> R {
+requires(!std::derived_from<Remove_cvptr_t<P>,
+                            std::enable_shared_from_this<Remove_cvptr_t<P>>> ||
+         std::derived_from<Replace_ptr_value_type_t<RP, void>,
+                           std::shared_ptr<void>>) constexpr auto clone
+    [[deprecated(/*u8*/ "Unsafe"), nodiscard]] (P &&ptr) -> R {
   if constexpr (!std::is_reference_v<P>) {
     if (!ptr) {
       if constexpr (std::is_pointer_v<R>) {
@@ -57,11 +69,11 @@ constexpr auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] (P &&ptr) -> R {
     }
   }
   if constexpr (std::is_pointer_v<R>) {
-    return &detail::clone_raw(ptr);
+    return &detail::clone_raw(std::forward<P>(ptr));
   } else if constexpr (std::is_reference_v<R>) {
-    return detail::clone_raw(ptr);
+    return detail::clone_raw(std::forward<P>(ptr));
   } else {
-    return R{&detail::clone_raw(ptr)};
+    return R{&detail::clone_raw(std::forward<P>(ptr))};
   }
 }
 template <cloneable P> auto clone_unique [[nodiscard]] (P &&ptr) {
