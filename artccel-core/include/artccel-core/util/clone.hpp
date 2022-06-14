529eecac7e51c605f172a168ae498a4fbe227dc1
William So <polyipseity@gmail.com>
2022-06-14 23:46:14 +0800

    Allow custom clone functions in 'util/clone.hpp'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/clone.hpp
+++ artccel-core/include/artccel-core/util/clone.hpp
@@ -5,58 +5,72 @@
 #include "encoding.hpp" // import c8srtombs
 #include "meta.hpp"     // import Remove_cvptr_t, Replace_ptr_value_type_t
 #include <cassert>      // import assert
-#include <concepts>     // import std::convertible_to, std::derived_from
-#include <exception>    // import std::invalid_argument
+#include <concepts> // import std::convertible_to, std::derived_from, std::invocable
+#include <exception>  // import std::invalid_argument
+#include <functional> // import std::invoke
 #include <memory> // import std::enable_shared_from_this, std::shared_ptr, std::unique_ptr
-#include <type_traits> // import std::is_pointer_v, std::is_reference_v, std::remove_reference_t
-#include <utility> // import std::declval, std::forward
+#include <type_traits> // import std::invoke_result_t, std::is_pointer_v, std::is_reference_v, std::remove_reference_t
+#include <utility>     // import std::declval, std::forward
 
 namespace artccel::core::util {
-template <typename P>
-concept cloneable = requires(Remove_cvptr_t<P> const &ptr) {
+template <typename P, typename F>
+concept cloneable = requires(Remove_cvptr_t<P> const &ptr, F &&func) {
+  requires std::invocable<F, decltype(ptr)>;
   requires std::convertible_to < Remove_cvptr_t<P>
-  &, Remove_cvptr_t<decltype(ptr.clone())> & > ;
+  &, Remove_cvptr_t<decltype(std::invoke(func, ptr))> & > ;
 };
+template <typename P>
+constexpr auto default_clone_function{[]() noexcept {
+  constexpr struct {
+    constexpr auto operator()
+        [[deprecated, nodiscard]] (Remove_cvptr_t<P> const &ptr) const
+        -> decltype(auto) {
+      return ptr.clone();
+    }
+  } init{};
+  return init;
+}()};
 
 namespace detail {
-template <cloneable P>
-constexpr auto clone_raw [[deprecated, nodiscard]] (P &&ptr) -> auto & {
+template <typename F, cloneable<F> P>
+constexpr auto clone_raw [[deprecated, nodiscard]] (P &&ptr, F &&func)
+-> auto & {
+  using return_type = std::invoke_result_t<F, Remove_cvptr_t<P> const &>;
   if constexpr (std::is_reference_v<P>) {
-    using return_type = decltype(ptr.clone());
     if constexpr (std::is_pointer_v<return_type>) {
-      return *(ptr.clone());
+      return *std::invoke(std::forward<F>(func), ptr);
     } else if constexpr (std::is_reference_v<return_type>) {
-      return ptr.clone();
+      return std::invoke(std::forward<F>(func), ptr);
     } else {
-      return ptr.clone().release();
+      return std::invoke(std::forward<F>(func), ptr).release();
     }
   } else {
     // clang-format off
-      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay, hicpp-no-array-decay)
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay, hicpp-no-array-decay)
     /* clang-format on */ assert(ptr && u8"ptr == nullptr");
-    using return_type = decltype(ptr->clone());
     if constexpr (std::is_pointer_v<return_type>) {
-      return *(ptr->clone());
+      return *std::invoke(std::forward<F>(func), *ptr);
     } else if constexpr (std::is_reference_v<return_type>) {
-      return ptr->clone();
+      return std::invoke(std::forward<F>(func), *ptr);
     } else {
-      return ptr->clone().release();
+      return std::invoke(std::forward<F>(func), *ptr).release();
     }
   }
 }
 } // namespace detail
 
 template <
-    cloneable P, typename RP = P,
+    typename F, cloneable<F> P, typename RP = P,
     typename R = Replace_ptr_value_type_t<
-        RP, std::remove_reference_t<
-                // NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations)
-                decltype(detail::clone_raw(std::declval<P>()))>>>
+        RP,
+        std::remove_reference_t<
+            // NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations)
+            decltype(detail::clone_raw(std::declval<P>(), std::declval<F>()))>>>
 requires(!std::derived_from<Remove_cvptr_t<P>,
                             std::enable_shared_from_this<Remove_cvptr_t<P>>> ||
          std::derived_from<Replace_ptr_value_type_t<RP, void>,
                            std::shared_ptr<void>>) constexpr auto clone
-    [[deprecated(/*u8*/ "Unsafe"), nodiscard]] (P &&ptr) -> R {
+    [[deprecated(/*u8*/ "Unsafe"), nodiscard]] (P &&ptr, F &&func) -> R {
   if constexpr (!std::is_reference_v<P>) {
     if (!ptr) {
       if constexpr (std::is_pointer_v<R>) {
@@ -69,20 +83,40 @@ requires(!std::derived_from<Remove_cvptr_t<P>,
     }
   }
   if constexpr (std::is_pointer_v<R>) {
-    return &detail::clone_raw(std::forward<P>(ptr));
+    return &detail::clone_raw(std::forward<P>(ptr), std::forward<F>(func));
   } else if constexpr (std::is_reference_v<R>) {
-    return detail::clone_raw(std::forward<P>(ptr));
+    return detail::clone_raw(std::forward<P>(ptr), std::forward<F>(func));
   } else {
-    return R{&detail::clone_raw(std::forward<P>(ptr))};
+    return R{&detail::clone_raw(std::forward<P>(ptr), std::forward<F>(func))};
   }
 }
-template <cloneable P> auto clone_unique [[nodiscard]] (P &&ptr) {
-  // NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations)
-  return clone<P, std::unique_ptr<void>>(std::forward<P>(ptr));
+template <typename P>
+requires cloneable<P, decltype(default_clone_function<P>)>
+auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] (P &&ptr)
+-> decltype(auto) {
+  return clone(std::forward<P>(ptr), default_clone_function<P>);
 }
-template <cloneable P> auto clone_shared [[nodiscard]] (P &&ptr) {
+template <typename F, cloneable<F> P>
+auto clone_unique [[nodiscard]] (P &&ptr, F &&func) -> decltype(auto) {
   // NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations)
-  return clone<P, std::shared_ptr<void>>(std::forward<P>(ptr));
+  return clone<F, P, std::unique_ptr<void>>(std::forward<P>(ptr),
+                                            std::forward<F>(func));
+}
+template <typename P>
+requires cloneable<P, decltype(default_clone_function<P>)>
+auto clone_unique [[nodiscard]] (P &&ptr) -> decltype(auto) {
+  return clone_unique(std::forward<P>(ptr), default_clone_function<P>);
+}
+template <typename F, cloneable<F> P>
+auto clone_shared [[nodiscard]] (P &&ptr, F &&func) -> decltype(auto) {
+  // NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations)
+  return clone<F, P, std::shared_ptr<void>>(std::forward<P>(ptr),
+                                            std::forward<F>(func));
+}
+template <typename P>
+requires cloneable<P, decltype(default_clone_function<P>)>
+auto clone_shared [[nodiscard]] (P &&ptr) -> decltype(auto) {
+  return clone_shared(std::forward<P>(ptr), default_clone_function<P>);
 }
 } // namespace artccel::core::util
 
