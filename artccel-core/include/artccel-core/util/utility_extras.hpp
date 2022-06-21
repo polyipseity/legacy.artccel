f26a98eb05434af5481ef08ed1b5cec371e59fdf
William So <polyipseity@gmail.com>
2022-06-21 14:00:39 +0800

    Create 'artccel::core::util::possible_ref_as_pointer'
    - Fix the return type of 'artccel::core::util::forward_apply'.
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/utility_extras.hpp
+++ artccel-core/include/artccel-core/util/utility_extras.hpp
@@ -5,10 +5,23 @@
 #include <concepts>    // import std::invocable
 #include <cstddef>     // import std::size_t
 #include <functional>  // import std::invoke
-#include <type_traits> // import std::invoke_result_t, std::is_nothrow_invocable_v, std::is_nothrow_move_constructible_v
+#include <type_traits> // import std::invoke_result_t, std::is_nothrow_invocable_v, std::is_nothrow_move_constructible_v, std::is_reference_v
 #include <utility>     // import std::index_sequence, std::index_sequence_for
 
 namespace artccel::core::util {
+template <typename> constexpr inline auto dependent_false_v{false};
+
+template <typename T>
+constexpr auto possible_ref_as_pointer(T &&value) noexcept {
+  if constexpr (std::is_reference_v<T>) {
+    // lvalues (T = t&), xvalues (T = t&&)
+    return &value;
+  } else {
+    // prvalues (T = t), others
+    return value;
+  }
+}
+
 template <typename F, template <typename...> typename Tuple, typename... Args>
 requires std::invocable<F, Args...>
 constexpr auto forward_apply(F &&func, Tuple<Args...> &&t_args) noexcept(
@@ -16,16 +29,18 @@ constexpr auto forward_apply(F &&func, Tuple<Args...> &&t_args) noexcept(
         std::is_nothrow_move_constructible_v<std::invoke_result_t<F, Args...>>)
     -> decltype(auto) {
   using TArgs = Tuple<Args...>;
-  return []<std::size_t... I>(
-      F && func, TArgs && t_args,
-      [[maybe_unused]] std::index_sequence<
-          I...> /*unused*/) mutable noexcept(std::
-                                                 is_nothrow_invocable_v<
-                                                     F, Args...> &&
-                                             std::
-                                                 is_nothrow_move_constructible_v<
-                                                     std::invoke_result_t<
-                                                         F, Args...>>) {
+  return
+      []<std::size_t... I>(
+          F && func, TArgs && t_args,
+          [[maybe_unused]] std::index_sequence<
+              I...> /*unused*/) mutable noexcept(std::
+                                                     is_nothrow_invocable_v<
+                                                         F, Args...> &&
+                                                 std::
+                                                     is_nothrow_move_constructible_v<
+                                                         std::invoke_result_t<
+                                                             F, Args...>>)
+          ->decltype(auto) {
     return std::invoke(
         std::forward<F>(func),
         std::forward<Args>(std::get<I>(std::forward<TArgs>(t_args)))...);
