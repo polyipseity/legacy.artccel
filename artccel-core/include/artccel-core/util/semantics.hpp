1f1926d29017fe9c21dc62d40bd3b494d63b0144
William So <polyipseity@gmail.com>
2022-06-21 20:11:58 +0800

    Remove 'artccel::core::util::Owner' in 'util/semantics.hpp'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/semantics.hpp
+++ artccel-core/include/artccel-core/util/semantics.hpp
@@ -9,10 +9,6 @@ namespace artccel::core::util {
 using Cu8zstring = gsl::basic_zstring<char8_t const, gsl::dynamic_extent>;
 using U8zstring = gsl::basic_zstring<char8_t, gsl::dynamic_extent>;
 
-template <typename T>
-requires std::is_reference_v<T>
-using Owner = T;
-
 template <typename T> using Observer_ptr = T *;
 template <typename T> using Observer_ref = T &;
 } // namespace artccel::core::util
