77dd8906e146e8cb68b3a33fc5350eaf9e633878
William So <polyipseity@gmail.com>
2022-06-22 17:04:54 +0800

    Create 'geometry/geometry.hpp' and 'geometry/geometry_impl.hpp'
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ artccel-core/include/artccel-core/geometry/geometry.hpp
@@ -0,0 +1,91 @@
+#ifndef ARTCCEL_CORE_GEOMETRY_GEOMETRY_HPP
+#define ARTCCEL_CORE_GEOMETRY_GEOMETRY_HPP
+#pragma once
+
+#include "../util/interval.hpp"  // import util::Nonnegative_interval
+#include "../util/semantics.hpp" // import util::Observer_ptr
+#include <cinttypes>             // import std::int8_t
+#include <concepts>              // import std::derived_from
+#include <gsl/gsl>               // import gsl::owner
+#include <typeinfo>              // import typeid, std::type_info
+
+namespace artccel::core::geometry {
+class Geometry;
+class Primitive;
+class Point;
+
+class Quality;
+
+class Quality {
+public:
+  virtual ~Quality() noexcept = default;
+  Quality(Quality const &) = delete;
+  auto operator=(Quality const &) = delete;
+  Quality(Quality &&) = delete;
+  auto operator=(Quality &&) = delete;
+
+protected:
+  Quality() noexcept = default;
+};
+
+class Geometry {
+public:
+  virtual auto dimension [[nodiscard]] () const
+      -> util::Nonnegative_interval<std::int8_t> = 0;
+  template <std::derived_from<Quality> T>
+  auto try_get_quality [[nodiscard]] () {
+    return dynamic_cast<util::Observer_ptr<T>>(try_get_quality(typeid(T)));
+  }
+  template <std::derived_from<Quality> T>
+  auto try_get_quality [[nodiscard]] () const {
+    return dynamic_cast<util::Observer_ptr<T const>>(
+        try_get_quality(typeid(T)));
+  }
+  virtual auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] () const
+      -> gsl::owner<Geometry *> = 0;
+  virtual ~Geometry() noexcept = default;
+  Geometry(Geometry const &) = delete;
+  auto operator=(Geometry const &) = delete;
+  Geometry(Geometry &&) = delete;
+  auto operator=(Geometry &&) = delete;
+
+protected:
+  Geometry() noexcept = default;
+  virtual auto try_get_quality
+      [[nodiscard]] (std::type_info const &quality_type)
+      -> util::Observer_ptr<Quality> = 0;
+  virtual auto try_get_quality
+      [[nodiscard]] (std::type_info const &quality_type) const
+      -> util::Observer_ptr<Quality const> = 0;
+};
+
+class Primitive : public virtual Geometry {
+public:
+  auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] () const
+      -> gsl::owner<Primitive *> override = 0;
+  ~Primitive() noexcept override = default;
+  Primitive(Primitive const &) = delete;
+  auto operator=(Primitive const &) = delete;
+  Primitive(Primitive &&) = delete;
+  auto operator=(Primitive &&) = delete;
+
+protected:
+  using Geometry::Geometry;
+};
+
+class Point : public virtual Primitive {
+public:
+  auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] () const
+      -> gsl::owner<Point *> override = 0;
+  ~Point() noexcept override = default;
+  Point(Point const &) = delete;
+  auto operator=(Point const &) = delete;
+  Point(Point &&) = delete;
+  auto operator=(Point &&) = delete;
+
+protected:
+  using Primitive::Primitive;
+};
+} // namespace artccel::core::geometry
+
+#endif
---
+++ artccel-core/include/artccel-core/geometry/geometry_impl.hpp
@@ -0,0 +1,129 @@
+#ifndef ARTCCEL_CORE_GEOMETRY_GEOMETRY_IMPL_HPP
+#define ARTCCEL_CORE_GEOMETRY_GEOMETRY_IMPL_HPP
+#pragma once
+
+#include "../util/interval.hpp"  // import util::Nonnegative_interval
+#include "../util/semantics.hpp" // import util::Observer_ptr
+#include "geometry.hpp"          // interface
+#include <array>                 // import std::array
+#include <cinttypes>             // import std::int8_t
+#include <concepts>              // import std::convertible_to
+#include <gsl/gsl>               // import gsl::owner
+#include <type_traits>           // import std::is_nothrow_copy_constructible_v
+#include <typeinfo>              // import std::type_info
+#include <utility>               // import std::swap
+
+namespace artccel::core::geometry::impl {
+namespace interface = geometry;
+
+template <util::Nonnegative_interval<std::int8_t> D>
+class Geometry : public virtual interface::Geometry {
+public:
+  // NOLINTNEXTLINE(fuchsia-statically-constructed-objects): constexpr ctor
+  constexpr static auto dimension_{D};
+  auto dimension [[nodiscard]] () const
+      -> util::Nonnegative_interval<std::int8_t> final {
+    return D;
+  }
+  ~Geometry() noexcept override = default;
+
+protected:
+  using interface::Geometry::Geometry;
+  void swap(Geometry &other [[maybe_unused]]) noexcept { using std::swap; }
+  Geometry(Geometry const &other [[maybe_unused]]) noexcept {};
+  auto operator=(Geometry const &right [[maybe_unused]]) noexcept(
+      noexcept(Geometry{right}.swap(*this), *this)) -> Geometry & {
+    Geometry{right}.swap(*this);
+    return *this;
+  }
+  Geometry(Geometry &&) noexcept = default;
+  auto operator=(Geometry &&right [[maybe_unused]]) noexcept -> Geometry & {
+    Geometry{std::move(right)}.swap(*this);
+    return *this;
+  }
+};
+
+template <util::Nonnegative_interval<std::int8_t> D>
+class Primitive : public virtual interface::Primitive, public Geometry<D> {
+public:
+  using Primitive::Geometry::dimension_;
+  ~Primitive() noexcept override = default;
+
+protected:
+  using Primitive::Geometry::Geometry;
+  void swap(Primitive &other) noexcept {
+    using std::swap;
+    Primitive::Geometry::swap(other);
+  }
+  Primitive(Primitive const &other) noexcept(
+      std::is_nothrow_copy_constructible_v<typename Primitive::Geometry>)
+      : Primitive::Geometry{other} {};
+  auto operator=(Primitive const &right) noexcept(
+      noexcept(Primitive{right}.swap(*this), *this)) -> Primitive & {
+    Primitive{right}.swap(*this);
+    return *this;
+  }
+  Primitive(Primitive &&) noexcept = default;
+  auto operator=(Primitive &&right) noexcept -> Primitive & {
+    Primitive{std::move(right)}.swap(*this);
+    return *this;
+  }
+};
+
+template <util::Nonnegative_interval<std::int8_t> D>
+class Point : public virtual interface::Point, public Primitive<D> {
+public:
+  using Point::Primitive::dimension_;
+
+private:
+  std::array<compute::Compute_out<double>, D> position_{};
+
+public:
+  Point() noexcept = default;
+  template <
+      std::convertible_to<typename decltype(position_)::value_type>... Position>
+  requires(sizeof...(Position) == D) explicit Point(Position... position)
+      : position_{std::move(position)...} {}
+  auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] () const
+      -> gsl::owner<interface::Point *> override {
+    return new Point{*this};
+  }
+  ~Point() noexcept override = default;
+
+protected:
+  auto try_get_quality [[nodiscard]] (std::type_info const &quality_type
+                                      [[maybe_unused]]) noexcept
+      -> util::Observer_ptr<Quality> override {
+    return nullptr;
+  }
+  auto try_get_quality [[nodiscard]] (std::type_info const &quality_type
+                                      [[maybe_unused]]) const noexcept
+      -> util::Observer_ptr<Quality const> override {
+    return nullptr;
+  }
+
+  void swap(Point &other) noexcept {
+    using std::swap;
+    Point::Primitive::swap(other);
+    swap(position_, other.position_);
+  }
+  Point(Point const &other) noexcept(
+      std::is_nothrow_copy_constructible_v<typename Point::Primitive>
+          &&noexcept(decltype(position_){other.position_}))
+      : Point::Primitive{other}, position_{other.position_} {}
+  auto operator=(Point const &right) noexcept(noexcept(Point{right}.swap(*this),
+                                                       *this)) -> Point & {
+    Point{right}.swap(*this);
+    return *this;
+  }
+  Point(Point &&other) noexcept
+      : Point::Primitive{std::move(other)}, position_{
+                                                std::move(other.position_)} {}
+  auto operator=(Point &&right) noexcept -> Point & {
+    Point{std::move(right)}.swap(*this);
+    return *this;
+  }
+};
+} // namespace artccel::core::geometry::impl
+
+#endif
