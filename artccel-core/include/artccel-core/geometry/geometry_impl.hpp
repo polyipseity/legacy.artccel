4938e8b00336db0d2d38affe9a4a5ba336684f83
William So <polyipseity@gmail.com>
2022-06-27 18:30:31 +0800

    Rename types in 'geometry/geometry_impl.hpp' to avoid name collisions
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/geometry/geometry_impl.hpp
+++ artccel-core/include/artccel-core/geometry/geometry_impl.hpp
@@ -17,7 +17,7 @@ namespace artccel::core::geometry::impl {
 namespace interface = geometry;
 
 template <util::Nonnegative_interval<std::int8_t> D>
-class Geometry : public virtual interface::Geometry {
+class Geometry_impl : public virtual interface::Geometry {
 public:
   // NOLINTNEXTLINE(fuchsia-statically-constructed-objects): constexpr ctor
   constexpr static auto dimension_{D};
@@ -25,70 +25,73 @@ public:
       -> util::Nonnegative_interval<std::int8_t> final {
     return D;
   }
-  ~Geometry() noexcept override = default;
+  ~Geometry_impl() noexcept override = default;
 
 protected:
   using interface::Geometry::Geometry;
-  void swap(Geometry &other [[maybe_unused]]) noexcept { using std::swap; }
-  Geometry(Geometry const &other [[maybe_unused]]) noexcept {};
-  auto operator=(Geometry const &right [[maybe_unused]]) noexcept(
-      noexcept(Geometry{right}.swap(*this), *this)) -> Geometry & {
-    Geometry{right}.swap(*this);
+  void swap(Geometry_impl &other [[maybe_unused]]) noexcept { using std::swap; }
+  Geometry_impl(Geometry_impl const &other [[maybe_unused]]) noexcept {};
+  auto operator=(Geometry_impl const &right [[maybe_unused]]) noexcept(
+      noexcept(Geometry_impl{right}.swap(*this), *this)) -> Geometry_impl & {
+    Geometry_impl{right}.swap(*this);
     return *this;
   }
-  Geometry(Geometry &&) noexcept = default;
-  auto operator=(Geometry &&right [[maybe_unused]]) noexcept -> Geometry & {
-    Geometry{std::move(right)}.swap(*this);
+  Geometry_impl(Geometry_impl &&) noexcept = default;
+  auto operator=(Geometry_impl &&right [[maybe_unused]]) noexcept
+      -> Geometry_impl & {
+    Geometry_impl{std::move(right)}.swap(*this);
     return *this;
   }
 };
 
 template <util::Nonnegative_interval<std::int8_t> D>
-class Primitive : public virtual interface::Primitive, public Geometry<D> {
+class Primitive_impl : public virtual interface::Primitive,
+                       public Geometry_impl<D> {
 public:
-  using Primitive::Geometry::dimension_;
-  ~Primitive() noexcept override = default;
+  using Primitive_impl::Geometry_impl::dimension_;
+  ~Primitive_impl() noexcept override = default;
 
 protected:
-  using Primitive::Geometry::Geometry;
-  void swap(Primitive &other) noexcept {
+  using Primitive_impl::Geometry_impl::Geometry_impl;
+  void swap(Primitive_impl &other) noexcept {
     using std::swap;
-    Primitive::Geometry::swap(other);
+    Primitive_impl::Geometry_impl::swap(other);
   }
-  Primitive(Primitive const &other) noexcept(
-      std::is_nothrow_copy_constructible_v<typename Primitive::Geometry>)
-      : Primitive::Geometry{other} {};
-  auto operator=(Primitive const &right) noexcept(
-      noexcept(Primitive{right}.swap(*this), *this)) -> Primitive & {
-    Primitive{right}.swap(*this);
+  Primitive_impl(Primitive_impl const &other) noexcept(
+      std::is_nothrow_copy_constructible_v<
+          typename Primitive_impl::Geometry_impl>)
+      : Primitive_impl::Geometry_impl{other} {};
+  auto operator=(Primitive_impl const &right) noexcept(
+      noexcept(Primitive_impl{right}.swap(*this), *this)) -> Primitive_impl & {
+    Primitive_impl{right}.swap(*this);
     return *this;
   }
-  Primitive(Primitive &&) noexcept = default;
-  auto operator=(Primitive &&right) noexcept -> Primitive & {
-    Primitive{std::move(right)}.swap(*this);
+  Primitive_impl(Primitive_impl &&) noexcept = default;
+  auto operator=(Primitive_impl &&right) noexcept -> Primitive_impl & {
+    Primitive_impl{std::move(right)}.swap(*this);
     return *this;
   }
 };
 
 template <util::Nonnegative_interval<std::int8_t> D>
-class Point : public virtual interface::Point, public Primitive<D> {
+class Point_impl : public virtual interface::Point, public Primitive_impl<D> {
 public:
-  using Point::Primitive::dimension_;
+  using Point_impl::Primitive_impl::dimension_;
 
 private:
   std::array<compute::Compute_out<double>, D> position_{};
 
 public:
-  explicit Point() noexcept = default;
+  explicit Point_impl() noexcept = default;
   template <
       std::convertible_to<typename decltype(position_)::value_type>... Position>
-  requires(sizeof...(Position) == D) explicit Point(Position... position)
+  requires(sizeof...(Position) == D) explicit Point_impl(Position... position)
       : position_{{std::move(position)...}} {}
   auto clone [[deprecated(/*u8*/ "Unsafe"), nodiscard]] () const
-      -> gsl::owner<interface::Point *> override {
-    return new Point{*this};
+      -> gsl::owner<Point_impl *> override {
+    return new Point_impl{*this};
   }
-  ~Point() noexcept override = default;
+  ~Point_impl() noexcept override = default;
 
 protected:
   auto try_get_quality [[nodiscard]] (std::type_info const &quality_type
@@ -102,25 +105,25 @@ protected:
     return nullptr;
   }
 
-  void swap(Point &other) noexcept {
+  void swap(Point_impl &other) noexcept {
     using std::swap;
-    Point::Primitive::swap(other);
+    Point_impl::Primitive_impl::swap(other);
     swap(position_, other.position_);
   }
-  Point(Point const &other) noexcept(
-      std::is_nothrow_copy_constructible_v<typename Point::Primitive>
+  Point_impl(Point_impl const &other) noexcept(
+      std::is_nothrow_copy_constructible_v<typename Point_impl::Primitive_impl>
           &&noexcept(decltype(position_){other.position_}))
-      : Point::Primitive{other}, position_{other.position_} {}
-  auto operator=(Point const &right) noexcept(noexcept(Point{right}.swap(*this),
-                                                       *this)) -> Point & {
-    Point{right}.swap(*this);
+      : Point_impl::Primitive_impl{other}, position_{other.position_} {}
+  auto operator=(Point_impl const &right) noexcept(
+      noexcept(Point_impl{right}.swap(*this), *this)) -> Point_impl & {
+    Point_impl{right}.swap(*this);
     return *this;
   }
-  Point(Point &&other) noexcept
-      : Point::Primitive{std::move(other)}, position_{
-                                                std::move(other.position_)} {}
-  auto operator=(Point &&right) noexcept -> Point & {
-    Point{std::move(right)}.swap(*this);
+  Point_impl(Point_impl &&other) noexcept
+      : Point_impl::Primitive_impl{std::move(other)}, position_{std::move(
+                                                          other.position_)} {}
+  auto operator=(Point_impl &&right) noexcept -> Point_impl & {
+    Point_impl{std::move(right)}.swap(*this);
     return *this;
   }
 };
