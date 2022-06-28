99b03ab4611e96086fed375b1709de33a45d664a
William So <polyipseity@gmail.com>
2022-06-28 10:16:25 +0800

    Enable interprocedural optimization in CMake
    Signed-off-by: William So <polyipseity@gmail.com>

--- build_auto/init.cmake
+++ build_auto/init.cmake
@@ -6,6 +6,15 @@ set(CMAKE_C_STANDARD_REQUIRED true)
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED true)
 
+include(CheckIPOSupported)
+check_ipo_supported(RESULT IPO_SUPPORTED OUTPUT IPO_OUTPUT)
+if(IPO_SUPPORTED)
+	set(CMAKE_INTERPROCEDURAL_OPTIMIZATION true)
+	message(STATUS "Interprocedural optimization is enabled")
+else()
+	message(STATUS "Interprocedural optimization is not supported: ${IPO_OUTPUT}")
+endif()
+
 # Workaround: Make clang-tidy include non-default system headers
 set(CMAKE_EXPORT_COMPILE_COMMANDS true)
 set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES} ${CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES})
