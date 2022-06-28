a62ea34105a36e55a64cfcbf18991d398a516f5a
William So <polyipseity@gmail.com>
2022-06-28 19:10:51 +0800

    Reformat 'util/interval.hpp'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/interval.hpp
+++ artccel-core/include/artccel-core/util/interval.hpp
@@ -324,10 +324,10 @@ private:
   constexpr static void
   check(type const &value) noexcept(noexcept(L{} < value && u8"L >(=) value",
                                              value < R{} && u8"value >(=) R")) {
-// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
-assert(L{} < value && u8"L >(=) value");
-// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
-assert(value < R{} && u8"value >(=) R");
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+    assert(L{} < value && u8"L >(=) value");
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+    assert(value < R{} && u8"value >(=) R");
   }
 };
 } // namespace artccel::core::util
