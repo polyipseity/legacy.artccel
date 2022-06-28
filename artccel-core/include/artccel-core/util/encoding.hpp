d0e135b916c0c3e2d10df625e98a6fe5ccdf3797
William So <polyipseity@gmail.com>
2022-06-28 20:26:14 +0800

    Create compile-time 'utf8_compat_as_utf8' and 'utf8_as_utf8_compat'
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/include/artccel-core/util/encoding.hpp
+++ artccel-core/include/artccel-core/util/encoding.hpp
@@ -9,45 +9,41 @@
 #include <array> // import std::array, std::begin, std::cbegin, std::size
 #include <string> // import std::string, std::u16string, std::u32string, std::u8string
 #include <string_view> // import std::string_view, std::u16string_view, std::u32string_view, std::u8string_view
-#include <utility>     // import std::move
+#include <utility>     // import std::as_const, std::move
 
 namespace artccel::core::util {
-namespace literals::encoding {
 namespace detail {
 template <typename AsCharT, Template_string Str>
 constexpr inline auto reinterpretation_storage{[] {
   std::array<AsCharT, std::size(Str.data_)> init{};
-  std::ranges::transform(Str.data_, std::begin(init), [](auto chr) noexcept {
-    return static_cast<AsCharT>(chr);
-  });
+  std::ranges::transform(
+      std::as_const(Str.data_), std::begin(init),
+      [](auto chr) noexcept { return static_cast<AsCharT>(chr); });
   return f::const_array(std::move(init));
 }()};
 } // namespace detail
-template <Template_string Str>
-constexpr auto operator""_as_utf8 [[nodiscard]] () {
+
+namespace f {
+auto utf8_compat_as_utf8(std::string_view utf8_compat) -> std::u8string;
+template <Template_string Str> consteval auto utf8_compat_as_utf8() {
   return std::u8string_view{
       std::cbegin(detail::reinterpretation_storage<char8_t, Str>),
       std::size(detail::reinterpretation_storage<char8_t, Str>) -
           null_terminator_size};
 }
-constexpr auto operator""_as_utf8 [[nodiscard]] (char utf8_compat) {
+constexpr auto utf8_compat_as_utf8(char utf8_compat) noexcept {
   return static_cast<char8_t>(utf8_compat);
 }
-template <Template_string Str>
-constexpr auto operator""_as_utf8_compat [[nodiscard]] () {
+auto utf8_as_utf8_compat(std::u8string_view utf8) -> std::string;
+template <Template_string Str> consteval auto utf8_as_utf8_compat() {
   return std::string_view{
       std::cbegin(detail::reinterpretation_storage<char, Str>),
       std::size(detail::reinterpretation_storage<char, Str>) -
           null_terminator_size};
 }
-constexpr auto operator""_as_utf8_compat [[nodiscard]] (char8_t utf8) {
+constexpr auto utf8_as_utf8_compat(char8_t utf8) noexcept {
   return static_cast<char>(utf8);
 }
-} // namespace literals::encoding
-
-namespace f {
-auto utf8_compat_as_utf8(std::string_view utf8_compat) -> std::u8string;
-auto utf8_as_utf8_compat(std::u8string_view utf8) -> std::string;
 
 auto utf8_to_utf16(std::u8string_view utf8) -> std::u16string;
 auto utf8_to_utf16(char8_t utf8) -> std::u16string;
@@ -67,6 +63,23 @@ auto utf16_to_loc_enc(char16_t utf16) -> std::string;
 auto utf32_to_loc_enc(std::u32string_view utf32) -> std::string;
 auto utf32_to_loc_enc(char32_t utf32) -> std::string;
 } // namespace f
+
+namespace literals::encoding {
+template <Template_string Str>
+constexpr auto operator""_as_utf8 [[nodiscard]] () {
+  return f::utf8_compat_as_utf8<Str>();
+}
+constexpr auto operator""_as_utf8 [[nodiscard]] (char utf8_compat) noexcept {
+  return f::utf8_compat_as_utf8(utf8_compat);
+}
+template <Template_string Str>
+constexpr auto operator""_as_utf8_compat [[nodiscard]] () {
+  return f::utf8_as_utf8_compat<Str>();
+}
+constexpr auto operator""_as_utf8_compat [[nodiscard]] (char8_t utf8) noexcept {
+  return f::utf8_as_utf8_compat(utf8);
+}
+} // namespace literals::encoding
 } // namespace artccel::core::util
 
 #endif
