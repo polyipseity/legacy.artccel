9b442391554b67cecdd2a5baf2fd9bc4f39ac671
William So <polyipseity@gmail.com>
2022-07-02 00:25:23 +0800

    Modify the build automation
    - Suppress C4068 for MSVC.
    - Generate 'export.h' for 'artccel-core' to properly export DLL symbols.
    - Set 'CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS' to true to export all symbols.
    - Include generated headers as system headers to suppress warnings.
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- build_auto/init.cmake
+++ build_auto/init.cmake
@@ -17,7 +17,9 @@ add_compile_options(
 	$<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wno-error=unknown-pragmas>
 	$<$<CXX_COMPILER_ID:MSVC>:/W4>
 	$<$<CXX_COMPILER_ID:MSVC>:/WX>
+	$<$<CXX_COMPILER_ID:MSVC>:/wd4068> # unknown pragma
 )
+
 include(CheckIPOSupported)
 check_ipo_supported(RESULT IPO_SUPPORTED OUTPUT IPO_OUTPUT)
 
@@ -27,6 +29,33 @@ if(IPO_SUPPORTED)
 else()
 	message(STATUS "Interprocedural optimization is not supported: ${IPO_OUTPUT}")
 endif()
+
+unset(IPO_SUPPORTED)
+
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
+include(GenerateExportHeader)
+
+function(generate_preset_export_header LIBRARY_TARGET)
+	string(REPLACE "-" "_" LIBRARY_TARGET_CODE "${LIBRARY_TARGET}")
+	string(TOUPPER "${LIBRARY_TARGET_CODE}" LIBRARY_TARGET_CODE_UPPER)
+	set(CUSTOM_EXPORT_HEADER "
+#ifdef ${LIBRARY_TARGET_CODE}_STATIC_DEFINE
+#	define ${LIBRARY_TARGET_CODE_UPPER}_EXPORT_DECLARATION
+#	define ${LIBRARY_TARGET_CODE_UPPER}_EXPORT_DEFINITION
+#else
+#	ifdef ${LIBRARY_TARGET_CODE}_EXPORTS
+#		define ${LIBRARY_TARGET_CODE_UPPER}_EXPORT_DECLARATION
+#		define ${LIBRARY_TARGET_CODE_UPPER}_EXPORT_DEFINITION __declspec(dllexport)
+#	else
+#		define ${LIBRARY_TARGET_CODE_UPPER}_EXPORT_DECLARATION __declspec(dllimport)
+#		define ${LIBRARY_TARGET_CODE_UPPER}_EXPORT_DEFINITION
+#	endif
+#endif
+")
+	generate_export_header("${LIBRARY_TARGET}"
+		EXPORT_FILE_NAME "include/${LIBRARY_TARGET}/export.h"
+		CUSTOM_CONTENT_FROM_VARIABLE CUSTOM_EXPORT_HEADER)
+endfunction()
 
 # Workaround: Make clang-tidy include non-default system headers
 set(CMAKE_EXPORT_COMPILE_COMMANDS true)
