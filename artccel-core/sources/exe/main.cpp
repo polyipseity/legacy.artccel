94637e5b066fba40a2697e253087c0ecbeae8bc5
William So <polyipseity@gmail.com>
2022-07-02 01:11:54 +0800

    Use 'gsl::final_action' to call 'artccel::core::f::main_cleanup' in the main function
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/sources/exe/main.cpp
+++ artccel-core/sources/exe/main.cpp
@@ -1,9 +1,12 @@
 #include <artccel-core/main_hooks.hpp> // import Arguments_t, artccel::core::f::safe_main, f::main_cleanup, f::main_setup
+#include <gsl/gsl>                     // import gsl::final_action
 
 namespace artccel::core::detail {
 static auto main_0(Arguments_t args) -> int {
   [[maybe_unused]] auto const [norm_args]{f::main_setup(args)};
-  [[maybe_unused]] auto const [placeholder]{f::main_cleanup(args)};
+  gsl::final_action const finalizer{[args] {
+    [[maybe_unused]] auto const [placeholder]{f::main_cleanup(args)};
+  }};
   return 0;
 }
 } // namespace artccel::core::detail
