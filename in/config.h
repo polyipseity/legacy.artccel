c08be4a7cc603a7f987c5724badeb1b214e6cdd1
William So <polyipseity@gmail.com>
2022-07-02 00:39:49 +0800

    Remove unnecessary 'NOLINT's in 'config.h's
    Signed-off-by: William So <polyipseity@gmail.com>

--- in/config.h
+++ in/config.h
@@ -2,11 +2,8 @@
 #define ARTCCEL_CONFIG_H
 #pragma once
 
-// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define ARTCCEL_VERSION_MAJOR /* clang-format off */@artccel_VERSION_MAJOR@ /* clang-format on */
-// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define ARTCCEL_VERSION_MINOR /* clang-format off */@artccel_VERSION_MINOR@ /* clang-format on */
-// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define ARTCCEL_VERSION_PATCH /* clang-format off */@artccel_VERSION_PATCH@ /* clang-format on */
 
 #endif
