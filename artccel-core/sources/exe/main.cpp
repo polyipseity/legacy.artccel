d17fc2cab624165065f028951b15364a4bb4ede8
William So <polyipseity@gmail.com>
2022-07-05 19:46:19 +0800

    Code 'artccel::core::detail::echo_cin()' in 'exe/main.cpp'
    - Use functions to split code.
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- artccel-core/sources/exe/main.cpp
+++ artccel-core/sources/exe/main.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>                   // import std::ranges::for_each
 #include <artccel-core/main_hooks.hpp> // import Argument::verbatim, Main_program, Raw_arguments, artccel::core::f::safe_main
-#include <artccel-core/util/encoding.hpp> // import util::literals::encoding::operator""_as_utf8_compat, util::f::utf8_as_utf8_compat
+#include <artccel-core/util/encoding.hpp> // import util::f::utf8_as_utf8_compat, util::literals::encoding::operator""_as_utf8_compat
 #include <artccel-core/util/exception_extras.hpp> // import util::Rethrow_on_lexical_scope_exit
 #include <artccel-core/util/meta.hpp>      // import util::Template_string
 #include <artccel-core/util/reflect.hpp>   // import util::f::type_name_array
@@ -8,16 +8,15 @@
 #include <artccel-core/util/utility_extras.hpp> // import util::Overloader
 #include <exception>   // import std::exception, std::exception_ptr
 #include <gsl/gsl>     // import gsl::index, gsl::not_null
-#include <iostream>    // import std::cout
+#include <iostream>    // import std::cin, std::cout, std::flush
+#include <string>      // import std::string
 #include <string_view> // import std::u8string_view
 #include <variant>     // import std::visit
 
 namespace artccel::core::detail {
 using util::literals::encoding::operator""_as_utf8_compat;
 
-static auto main_0(Raw_arguments arguments) -> int {
-  util::Rethrow_on_lexical_scope_exit lexical_rethrower{};
-  Main_program const program{lexical_rethrower.write(), arguments};
+static void print_args(Main_program const &program) {
   std::cout << u8"arguments:\n"_as_utf8_compat;
   std::ranges::for_each(program.arguments(), [index{gsl::index{0}}](
                                                  auto arg) mutable {
@@ -50,6 +49,21 @@ static auto main_0(Raw_arguments arguments) -> int {
         arg.utf8_or_exc());
   });
   std::cout.flush();
+}
+
+static auto echo_cin() {
+  std::cout << u8"echo in: "_as_utf8_compat << std::flush;
+  std::string in{};
+  std::cin >> in;
+  std::cout << u8"echo out: "_as_utf8_compat << in << u8'\n'_as_utf8_compat
+            << std::flush;
+}
+
+static auto main_0(Raw_arguments arguments) -> int {
+  util::Rethrow_on_lexical_scope_exit lexical_rethrower{};
+  Main_program const program{lexical_rethrower.write(), arguments};
+  print_args(program);
+  echo_cin();
   return 0;
 }
 } // namespace artccel::core::detail
