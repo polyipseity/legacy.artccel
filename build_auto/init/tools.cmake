0b4c665d844cffe6c9b09c38723e4f3974b18caf
William So <polyipseity@gmail.com>
2022-07-11 14:41:47 +0800

    Configure 'CMAKE_C_CLANG_TIDY' as well in 'build_auto/init/tools.cmake'
    Signed-off-by: William So <polyipseity@gmail.com>

--- build_auto/init/tools.cmake
+++ build_auto/init/tools.cmake
@@ -7,7 +7,17 @@ endif()
 # clang-tidy
 # workaround: make clang-tidy include non-default system headers
 set(CMAKE_EXPORT_COMPILE_COMMANDS true)
+list(INSERT CMAKE_C_STANDARD_INCLUDE_DIRECTORIES 0 ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES})
 list(INSERT CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES 0 ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES})
+set(CMAKE_C_CLANG_TIDY clang-tidy
+	-p "${CMAKE_BINARY_DIR}/compile_commands.json"
+	"-line-filter=[\
+{\"name\": \"config.h\", \"lines\": [[65535, 1]]},\
+{\"name\": \"export.h\", \"lines\": [[65535, 1]]},\
+{\"name\": \".h\"},\
+{\"name\": \".c\"},\
+]"
+	"--config-file=${ROOT_SOURCE_DIR}/.clang-tidy")
 set(CMAKE_CXX_CLANG_TIDY clang-tidy
 	-p "${CMAKE_BINARY_DIR}/compile_commands.json"
 	"-line-filter=[\
