c12a463d16e190a8ec64d8cdc84883c740324bdf
William So <polyipseity@gmail.com>
2022-07-13 16:25:05 +0800

    Replace two more 'if()'s with generator expressions in 'build.cmake'
    Signed-off-by: William So <polyipseity@gmail.com>

--- build_auto/init/build.cmake
+++ build_auto/init/build.cmake
@@ -3,61 +3,52 @@ set(CMAKE_C_VISIBILITY_PRESET "hidden")
 set(CMAKE_CXX_VISIBILITY_PRESET "hidden")
 set(CMAKE_VISIBILITY_INLINES_HIDDEN true)
 
-if("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
-	set(CXX_CLANG_LIKE_COMPILER true)
-else()
-	set(CXX_CLANG_LIKE_COMPILER false)
-endif()
-
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR CXX_CLANG_LIKE_COMPILER)
-	set(CXX_GCC_LIKE_COMPILER true)
-else()
-	set(CXX_GCC_LIKE_COMPILER false)
-endif()
+set(CXX_CLANG_LIKE_COMPILER_GENEXP "$<BOOL:$<FILTER:$<CXX_COMPILER_ID>,INCLUDE,Clang>>")
+set(CXX_GCC_LIKE_COMPILER_GENEXP "$<OR:$<BOOL:$<CXX_COMPILER_ID:GNU>>,${CXX_CLANG_LIKE_COMPILER_GENEXP}>")
 
 add_compile_definitions("$<$<BOOL:${WIN32}>:NOMINMAX>")
 add_compile_options(
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-Wall>"
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-Wextra>"
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-Wpedantic>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Weverything>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-Wall>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-Wextra>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-Wpedantic>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Weverything>"
 	"$<$<CXX_COMPILER_ID:MSVC>:/Wall>"
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-Werror>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-Werror>"
 	"$<$<CXX_COMPILER_ID:MSVC>:/WX>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-c++20-compat>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-c++98-c++11-c++14-compat>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-c++98-compat>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-c++98-compat-pedantic>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-ctad-maybe-unsupported>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-missing-variable-declarations>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-return-std-move-in-c++11>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-c++20-compat>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-c++98-c++11-c++14-compat>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-c++98-compat>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-c++98-compat-pedantic>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-ctad-maybe-unsupported>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-missing-variable-declarations>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-return-std-move-in-c++11>"
 	"$<$<CXX_COMPILER_ID:MSVC>:/wd4068>" # unknown pragma
 	"$<$<CXX_COMPILER_ID:MSVC>:/wd4464>" # relative include path contains '..'
 	"$<$<CXX_COMPILER_ID:MSVC>:/wd4514>" # unreferenced inline function has been removed
 	"$<$<CXX_COMPILER_ID:MSVC>:/wd4710>" # function not inlined
 	"$<$<CXX_COMPILER_ID:MSVC>:/wd5030>" # attribute is not recognized
 
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-Wno-error=attributes>"
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-Wno-error=unknown-pragmas>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-error=unknown-argument>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-error=unknown-attributes>"
-	"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-Wno-error=unknown-warning-option>"
-	"$<$<AND:$<BOOL:${CXX_CLANG_LIKE_COMPILER}>,$<BOOL:${WIN32}>>:-Wno-error=nonportable-system-include-path>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-Wno-error=attributes>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-Wno-error=unknown-pragmas>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-error=unknown-argument>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-error=unknown-attributes>"
+	"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-Wno-error=unknown-warning-option>"
+	"$<$<AND:${CXX_CLANG_LIKE_COMPILER_GENEXP},$<BOOL:${WIN32}>>:-Wno-error=nonportable-system-include-path>"
 
 	# #pragma warning(disable : XXXX) seems to be broken for below warnings
 	"$<$<CXX_COMPILER_ID:MSVC>:/wd4868>" # compiler may not enforce left-to-right evaluation order in braced initializer list
 	"$<$<CXX_COMPILER_ID:MSVC>:/wd5045>" # Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
 
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-finput-charset=UTF-8>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-finput-charset=UTF-8>"
 	"$<$<CXX_COMPILER_ID:MSVC>:/source-charset:UTF-8>"
-	"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-fexec-charset=UTF-8>"
+	"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-fexec-charset=UTF-8>"
 	"$<$<CXX_COMPILER_ID:MSVC>:/execution-charset:UTF-8>"
 )
 
 if(ARTCCEL_PROFILE_COMPILATION)
 	add_compile_options(
-		"$<$<BOOL:${CXX_GCC_LIKE_COMPILER}>:-ftime-report>"
-		"$<$<BOOL:${CXX_CLANG_LIKE_COMPILER}>:-ftime-trace>"
+		"$<${CXX_GCC_LIKE_COMPILER_GENEXP}:-ftime-report>"
+		"$<${CXX_CLANG_LIKE_COMPILER_GENEXP}:-ftime-trace>"
 	)
 	message(STATUS "Compilation profiling is enabled")
 
