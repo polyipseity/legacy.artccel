fbf2c15e30e09d5cee5971b61971090d031242e0
William So <polyipseity@gmail.com>
2022-07-13 13:22:07 +0800

    Make running clang-tidy optional
    Signed-off-by: William So <polyipseity@gmail.com>

--- build_auto/init/tools.cmake
+++ build_auto/init/tools.cmake
@@ -5,27 +5,66 @@ endif()
 
 # tools
 # clang-tidy
-# workaround: make clang-tidy include non-default system headers
-set(CMAKE_EXPORT_COMPILE_COMMANDS true)
-list(INSERT CMAKE_C_STANDARD_INCLUDE_DIRECTORIES 0 "" ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES})
-list(INSERT CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES 0 "" ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES})
-set(CMAKE_C_CLANG_TIDY clang-tidy
-	-p "${CMAKE_BINARY_DIR}/compile_commands.json"
-	"-line-filter=[\
-{\"name\": \"config.h\", \"lines\": [[65535, 1]]},\
-{\"name\": \"export.h\", \"lines\": [[65535, 1]]},\
-{\"name\": \".h\"},\
-{\"name\": \".c\"},\
-]"
-	"--config-file=${ROOT_SOURCE_DIR}/.clang-tidy")
-set(CMAKE_CXX_CLANG_TIDY clang-tidy
-	-p "${CMAKE_BINARY_DIR}/compile_commands.json"
-	"-line-filter=[\
-{\"name\": \"config.h\", \"lines\": [[65535, 1]]},\
-{\"name\": \"export.h\", \"lines\": [[65535, 1]]},\
-{\"name\": \".h\"},\
-{\"name\": \".hpp\"},\
-{\"name\": \".c\"},\
-{\"name\": \".cpp\"}\
-]"
-	"--config-file=${ROOT_SOURCE_DIR}/.clang-tidy")
+function(target_integrate_clang_tidy TARGET LANGUAGE LINE_FILTER_EXCLUDES)
+	# workaround: make up for a lack of a usable filter
+	set(LINE_FILTER "")
+
+	foreach(LINE_FILTER_EXCLUDE IN LISTS LINE_FILTER_EXCLUDES)
+		string(APPEND LINE_FILTER "{\"name\": \"${LINE_FILTER_EXCLUDE}\", \"lines\": [[2, 1]]},")
+	endforeach()
+
+	string(APPEND LINE_FILTER "{\"name\": \".h\"}, {\"name\": \".c\"},")
+
+	if(LANGUAGE STREQUAL "CXX")
+		string(APPEND LINE_FILTER "\
+{\"name\": \".hpp\"}, {\"name\": \".cpp\"},\
+{\"name\": \".hxx\"}, {\"name\": \".cxx\"},\
+{\"name\": \".hh\"}, {\"name\": \".cc\"},\
+{\"name\": \".h++\"}, {\"name\": \".c++\"},\
+")
+	endif()
+
+	# actual work
+	string(REPLACE "X" "+" LANGUAGE_PROPER_NAME "${LANGUAGE}")
+	string(TOLOWER "${LANGUAGE_PROPER_NAME}" LANGUAGE_PROPER_NAME)
+	set(INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:${TARGET},INCLUDE_DIRECTORIES>")
+	set(CLANG_TIDY_OUTPUTS "")
+	get_target_property(TARGET_SOURCES "${TARGET}" SOURCES)
+
+	foreach(TARGET_SOURCE IN LISTS TARGET_SOURCES)
+		get_filename_component(TARGET_SOURCE_REAL "${TARGET_SOURCE}" REALPATH BASE_DIR "${PROJECT_SOURCE_DIR}")
+		string(SHA3_512 TARGET_SOURCE_HASH "${TARGET_SOURCE_REAL}")
+		add_custom_command(
+			OUTPUT "clang-tidy/${TARGET_SOURCE_HASH}.timestamp"
+			COMMAND clang-tidy ARGS
+			"--config-file=${ROOT_SOURCE_DIR}/.clang-tidy"
+			"--line-filter=[${LINE_FILTER}]"
+			"${TARGET_SOURCE_REAL}"
+			--
+			"$<$<BOOL:${INCLUDE_DIRECTORIES}>:-I$<JOIN:${INCLUDE_DIRECTORIES},;-I>>"
+			"-std=${LANGUAGE_PROPER_NAME}${CMAKE_${LANGUAGE}_STANDARD}"
+			-Wall -Wextra -Wpedantic -Werror # -Weverything has checks that crash clang-tidy
+			-finput-charset=UTF-8 -fexec-charset=UTF-8
+
+			# workaround: make clang-tidy include non-default system headers
+			"$<$<BOOL:${CMAKE_${LANGUAGE}_IMPLICIT_INCLUDE_DIRECTORIES}>:-isystem$<JOIN:${CMAKE_${LANGUAGE}_IMPLICIT_INCLUDE_DIRECTORIES},;-isystem>>"
+			COMMAND "${CMAKE_COMMAND}" ARGS -E touch "${PROJECT_BINARY_DIR}/clang-tidy/${TARGET_SOURCE_HASH}.timestamp"
+			DEPENDS "${TARGET_SOURCE_REAL}" # MAIN_DEPENDENCY silently overwrites compilation
+			IMPLICIT_DEPENDS "${LANGUAGE}" "${TARGET_SOURCE_REAL}"
+			WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+			COMMENT "Running clang-tidy on '${TARGET_SOURCE}' of '${TARGET}'"
+			VERBATIM COMMAND_EXPAND_LISTS # dumb generator expression with spaces is a time waster
+		)
+		list(APPEND CLANG_TIDY_OUTPUTS "clang-tidy/${TARGET_SOURCE_HASH}.timestamp")
+	endforeach()
+
+	add_custom_target("${TARGET}-clang-tidy"
+		DEPENDS ${CLANG_TIDY_OUTPUTS}
+		COMMENT "Running clang-tidy on '${TARGET}'"
+		VERBATIM
+	)
+
+	if(ARTCCEL_RUN_CLANG_TIDY)
+		add_dependencies("${TARGET}" "${TARGET}-clang-tidy")
+	endif()
+endfunction()
