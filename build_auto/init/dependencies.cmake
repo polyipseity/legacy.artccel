f74747b1f8bf01fcd6ae6c60c92615a7a60f8fe1
William So <polyipseity@gmail.com>
2022-07-13 23:24:25 +0800

    Add dependency 'https://github.com/ofats/any_invocable.git' (commit 853a90ba1b573a957ea2a452404e0cc68ee0b6cc)
    Signed-off-by: William So <polyipseity@gmail.com>

--- build_auto/init/dependencies.cmake
+++ build_auto/init/dependencies.cmake
@@ -49,6 +49,20 @@ FetchContent_Declare(Microsoft.GSL
 	GIT_PROGRESS true
 	FIND_PACKAGE_ARGS
 )
+find_package_or_fetch_content(Microsoft.GSL GSL "GSL_INSTALL;${ARTCCEL_INSTALL};GSL_TEST;${ARTCCEL_TEST}" "4.0.0;EXACT;REQUIRED;CONFIG")
+
+FetchContent_Declare(ofats-any_invocable
+	GIT_REPOSITORY "https://github.com/ofats/any_invocable.git"
+	GIT_TAG "853a90ba1b573a957ea2a452404e0cc68ee0b6cc" # tag v1.0.0
+	GIT_SHALLOW true
+	GIT_PROGRESS true
+	FIND_PACKAGE_ARGS
+)
+FetchContent_MakeAvailable(ofats-any_invocable)
+FetchContent_GetProperties(ofats-any_invocable)
+add_library(ofats::any_invocable INTERFACE IMPORTED)
+target_include_directories(ofats::any_invocable INTERFACE "${ofats-any_invocable_SOURCE_DIR}/include")
+
 FetchContent_Declare(tl-expected
 	GIT_REPOSITORY "https://github.com/TartanLlama/expected.git"
 	GIT_TAG "3d741708b967b83ca1e2888239196c4a67f9f9b0" # tag v1.0.0
@@ -56,5 +70,4 @@ FetchContent_Declare(tl-expected
 	GIT_PROGRESS true
 	FIND_PACKAGE_ARGS
 )
-find_package_or_fetch_content(Microsoft.GSL GSL "GSL_INSTALL;${ARTCCEL_INSTALL};GSL_TEST;${ARTCCEL_TEST}" "4.0.0;EXACT;REQUIRED;CONFIG")
 find_package_or_fetch_content(tl-expected expected "EXPECTED_BUILD_TESTS;${ARTCCEL_TEST}" "1.0.0;EXACT;REQUIRED;CONFIG")
