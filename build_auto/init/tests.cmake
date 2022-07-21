78cc74418b6b8d5d109c936b042631b198e032e6
William So <polyipseity@gmail.com>
2022-07-22 01:22:51 +0800

    Cleanup 'tests.cmake'
    Signed-off-by: William So <polyipseity@gmail.com>

--- build_auto/init/tests.cmake
+++ build_auto/init/tests.cmake
@@ -7,12 +7,6 @@ if(NOT BUILD_TESTING)
 	set(ARTCCEL_TEST false)
 endif()
 
-if(ARTCCEL_TEST)
-	set(ARTCCEL_TEST_EXCLUDE_FROM_ALL "")
-else()
-	set(ARTCCEL_TEST_EXCLUDE_FROM_ALL "EXCLUDE_FROM_ALL")
-endif()
-
 add_custom_target("${ARTCCEL_TARGET_NAMESPACE}-tests"
 	COMMENT "Building all tests"
 	VERBATIM)
@@ -22,13 +16,10 @@ function(target_as_test target)
 		message(FATAL_ERROR "Not a target: ${target}")
 	endif()
 
-	if(ARTCCEL_TEST)
-		set(_target_exclude_from_all false)
-	else()
-		set(_target_exclude_from_all true)
+	if(NOT ARTCCEL_TEST)
+		set_target_properties("${target}" PROPERTIES EXCLUDE_FROM_ALL true)
 	endif()
 
-	set_target_properties("${target}" PROPERTIES EXCLUDE_FROM_ALL "${_target_exclude_from_all}")
 	add_dependencies("${ARTCCEL_TARGET_NAMESPACE}-tests" "${target}")
 	add_test(NAME "${target}" COMMAND "${target}")
 endfunction()
